d5d8011e977091f49c3b585f30f26c30f0737b62
ASHISH AJAY <163020665+AASHIX@users.noreply.github.com>
2026-05-12 13:52:31 -0400

    Removed the old program and uploaded the new programs from the local system

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,12 +7,3 @@ public class HelloWorld {
         System.out.println("Hello, World");
     }
 }
-//Name: Ashish Ajay
-//Date: 06-05-2026
-//Description: This program will display 'Hello World'
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello, World");
-    }
-}
